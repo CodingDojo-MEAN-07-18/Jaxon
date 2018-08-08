d328498a95559ff562ff9f3b6550131fbd1528ff
Jaxon Lee <jwlee@mix.wvu.edu>
2018-08-08 12:18:36 -0400

    Added service to Restful_Tasks_API

--- MongoDB/Restful_Task_API/public/src/app/app.component.spec.ts
+++ MongoDB/Restful_Task_API/public/src/app/app.component.spec.ts
@@ -13,7 +13,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'public'`, async(() => {
+  it(`should have as title 'MEAN'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('public');
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to public!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to MEAN!');
   }));
 });
