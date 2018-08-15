1327ea481bf34a18840ba37a384dca978d153e55
Jaxon Lee <jwlee@mix.wvu.edu>
2018-08-15 10:00:49 -0400

    Added UI functionality to Restful_Task_API

--- MongoDB/Restful_Task_API/public/src/app/app.component.ts
+++ MongoDB/Restful_Task_API/public/src/app/app.component.ts
@@ -9,16 +9,24 @@ import { HttpService } from './http.service';
 export class AppComponent implements OnInit {
   title = 'MEAN';
   tasks;
+  description;
   constructor(private _httpService: HttpService) { }
    // ngOnInit will run when the component is initialized, after the constructor method.
   ngOnInit() {
-    this.getTasksFromService();
+    // this.getTasksFromService();
   }
   getTasksFromService() {
     const observable = this._httpService.getTasks();
     observable.subscribe(data => {
       console.log('Got our tasks!', data);
       this.tasks = data;
+    });
+  }
+  getDescription(task_id) {
+    const observable = this._httpService.getTaskbyID(task_id);
+    observable.subscribe(data => {
+      console.log('Got our task', data);
+      this.description = data;
     });
   }
 }
