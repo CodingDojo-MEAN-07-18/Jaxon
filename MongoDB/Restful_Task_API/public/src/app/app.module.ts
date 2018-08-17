04c6a83934870ecb0f555a15a562532c4847ac21
Jaxon Lee <jwlee@mix.wvu.edu>
2018-08-17 12:55:00 -0400

    Modularized description

--- MongoDB/Restful_Task_API/public/src/app/app.module.ts
+++ MongoDB/Restful_Task_API/public/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { HttpService } from './http.service';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { TaskComponent } from './task/task.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ MongoDB/Restful_Task_API/public/src/app/task/task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core'; // add Input to our imports
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  @Input() taskToShow: any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
